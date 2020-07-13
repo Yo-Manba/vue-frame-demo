751fe93e908ef995a172b7abbf8210151b9dffaa
louisevi <15757113726@163.com>
2020-07-13 10:42:33 +0800

    将底部选项卡部分抽离成独立子组件

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <template>
     <!-- 整体页面 -->
     <div id="app">
-        <!-- 页面视口 -->
+        <!-- 一级页面视口(包含Header高度) -->
         <keep-alive :include="include">
             <router-view class="onePage" />
         </keep-alive>
         <!-- 底部导航栏 -->
-        <div id="nav" class="border-top">
-            <router-link replace to="/">
-                <div>
-                    <span class="iconfont iconweixin"></span>
-                    <div>首页</div>
-                </div>
-            </router-link>
-            <router-link replace to="/contacts">
-                <div>
-                    <span class="iconfont icontongxunlu"></span>
-                    <div>通讯录</div>
-                </div>
-            </router-link>
-            <router-link replace to="/discover">
-                <div>
-                    <span class="iconfont iconzhinanzhen"></span>
-                    <div>发现</div>
-                </div>
-            </router-link>
-            <router-link replace to="/mine">
-                <div>
-                    <span class="iconfont iconmy-fill"></span>
-                    <div>我的</div>
-                </div>
-            </router-link>
-        </div>
+        <BottomTabBar />
     </div>
 </template>
 
 <script>
+import BottomTabBar from './components/BottomTabBar';
+
 export default {
     data() {
         return {
             
         };
     },
+    components: {
+        BottomTabBar
+    },
+
     mounted() {
         
     },
@@ -74,41 +55,10 @@ body {
     left: 0;
 }
 
-// 一级页面视口
+// 一级页面视口(包含Header高度)
 .onePage {
-    // border: 1px solid red;
     height: calc(100% - 1.1rem);
     box-sizing: border-box;
 }
 
-// 底部导航栏
-#nav {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    box-sizing: border-box;
-    height: 1.1rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    text-align: center;
-    background-color: #f5f5f5;
-    z-index: 99;
-
-    a {
-        font-size: 0.24rem;
-        color: #666;
-
-        .iconfont {
-            font-size: 0.42rem;
-            display: block;
-            margin-bottom: 5px;
-        }
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
-}
 </style>
---
+++ src/components/BottomTabBar.vue
@@ -0,0 +1,77 @@
+
+<template>
+    <div id="nav" class="border-top">
+        <router-link replace to="/">
+            <div>
+                <span class="iconfont iconweixin"></span>
+                <div>首页</div>
+            </div>
+        </router-link>
+        <router-link replace to="/contacts">
+            <div>
+                <span class="iconfont icontongxunlu"></span>
+                <div>通讯录</div>
+            </div>
+        </router-link>
+        <router-link replace to="/discover">
+            <div>
+                <span class="iconfont iconzhinanzhen"></span>
+                <div>发现</div>
+            </div>
+        </router-link>
+        <router-link replace to="/mine">
+            <div>
+                <span class="iconfont iconmy-fill"></span>
+                <div>我的</div>
+            </div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data() {
+        return {};
+    },
+
+    components: {},
+
+    computed: {},
+
+    methods: {}
+};
+</script>
+
+<style lang='less' scoped>
+// 底部导航栏
+#nav {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    box-sizing: border-box;
+    height: 1.1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    text-align: center;
+    background-color: #f5f5f5;
+    z-index: 99;
+
+    a {
+        font-size: 0.24rem;
+        color: #666;
+
+        .iconfont {
+            font-size: 0.42rem;
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
+}
+</style>
